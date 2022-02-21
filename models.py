f41840922612fc9d0fc18ae918649ce59d1832b7
Fayyoz24 <fayyozusmon@gmail.com>
2022-02-21 14:19:09 +0100

    a

--- models.py
+++ models.py
@@ -1,26 +1,70 @@
+import pandas as pd
 import time
-from sklearn import model_selection
 from sklearn.model_selection import cross_val_score
 from sklearn.naive_bayes import GaussianNB
 from sklearn.linear_model import LogisticRegression
 from sklearn import tree
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.svm import SVC
-from sklearn import pipeline 
 from sklearn import metrics
 from xgboost import XGBClassifier
-from sklearn.preprocessing import StandardScaler, LabelEncoder
-from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.preprocessing import StandardScaler
+
+def get_model():
+
+  test_data1 = pd.read_csv("test_side1.csv")
+  test_data2 = pd.read_csv("test_side2.csv")
+  train_data = pd.read_csv("train_side.csv")
+
+  test_data1.target = test_data1.target.replace({'Car':0, 'Train':1, 'Bus':2, 'Still':3, 'Walking':4})
+  test_data2.target = test_data2.target.replace({'Car':0, 'Train':1, 'Bus':2, 'Still':3, 'Walking':4})
+  train_data.target = train_data.target.replace({'Car':0, 'Train':1, 'Bus':2, 'Still':3, 'Walking':4})
 
 
-def 
-    le = LabelEncoder()
+  x_train = train_data.drop(["target"], axis=1)
+  y_train = train_data.target
 
-    train_data.target = le.fit_transform(train_data.target)
-    train_data.user = le.fit_transform(train_data.user)
+  x_test1 = test_data1.drop(["target"], axis=1)
+  y_test1 = test_data1.target
 
-    test_data.target = le.fit_transform(test_data.target)
-    test_data.user = le.fit_transform(test_data.user)
+  x_test2 = test_data2.drop(["target"], axis=1)
+  y_test2 = test_data2.target
 
-    train_data
+  scaler = StandardScaler()
+  x_train_scaled = scaler.fit_transform(x_train)
+
+
+  tree_classifiers = {
+  "GaussianNB": GaussianNB(),
+  "Logistic Regression":   LogisticRegression(max_iter=2000),
+  "Decision Tree Classifier": tree.DecisionTreeClassifier(random_state=1),
+  "KNeighborsClassifier":      KNeighborsClassifier(),
+  "Random Forest Classifier":       RandomForestClassifier(random_state=1),
+  "XGBoost":       XGBClassifier(random_state = 1)
+  }
+
+  results = pd.DataFrame({'Model': [], 'Accuracy': [], 'Bal Acc.': [], 'Time': []})
+
+  results = pd.DataFrame({'Model': [], 'Accuracy': [], 'Bal Acc.': [], 'Time': []})
+  for model_name, model in tree_classifiers.items():
+
+      start_time = time.time()
+      model.fit(x_train_scaled, y_train)
+      total_time = time.time() - start_time
+          
+      pred = model.predict(x_test1)
+      
+      results = results.append({"Model":    model_name,
+                              "Accuracy": metrics.accuracy_score(y_test1, pred)*100,
+                              "Bal Acc.": metrics.balanced_accuracy_score(y_test1, pred)*100,
+                              "Time":     total_time},
+                              ignore_index=True)
+
+  return pd.read_csv('res.csv', index_col=False), round(pred.mean())
+
+
+
+    # results_ord = results.sort_values(by=['Accuracy'], ascending=False, ignore_index=True)
+    # results_ord.index += 1 
+    # results_ord.style.bar(subset=['Accuracy', 'Bal Acc.'], vmin=0, vmax=100, color='#5fba7d')
+print(get_model())
--- stream.py
+++ stream.py
@@ -1,3 +1,4 @@
+from re import I
 from turtle import color, width
 import streamlit as st
 import pandas as pd
@@ -6,16 +7,17 @@ from sklearn.preprocessing import StandardScaler as scaler
 import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.figure_factory as ff
+from models import get_model
 
 data = pd.read_csv('./last.csv')
 
 
-st.markdown("<h1 style='text-align: center; color: blue;'>time to remember TITANIC!...</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; color: blue;'>CALORIES!...</h1>", unsafe_allow_html=True)
 img = Image.open("calorie.png")
 st.image(img, width=600)
 
 
-images = st.sidebar.radio('EDA and FE', options=('yes', 'no'))
+images = st.sidebar.radio('EDA and FE', options=('no', 'yes'))
 if images == 'yes':
 
     # fig, ax = plt.subplots(figsize=(20,20))
@@ -36,9 +38,14 @@ if images == 'yes':
     img2 = Image.open('third.png')
     st.image(img2, width=800)
     
- 
 
+mode, result = get_model()
 
+user_ = st.sidebar.radio(
+    label="Check accuracy data!",
+    options=("No",'Yes'))
+if user_ == 'Yes':
+    st.write(mode)
 
 try: 
     user_input = st.sidebar.radio(
@@ -47,16 +54,18 @@ try:
 
     if user_input == 'Yes':
 
-        weight = st.text_input("Enter your weight! (in kg) \n")
+        weight = int(st.text_input("Enter your weight! (in kg) \n"))
 
-        height = st.text_input("height! (in sm) \n")
+        height = int(st.text_input("height! (in sm) \n"))
       
         predict1 = st.button("predict")
         #pred = models()
-        pred = 'walking'
+# 'Car':0, 'Train':1, 'Bus':2, 'Still':3, 'Walking':4
+
+        pred = result
         if predict1:
 
-            if pred == 'walking':
+            if pred == 4:
                 # """"
                 # Multiply your body weight in kilograms by 0.035. Eg. 0.035 x 60 = 2.1
                 # Square your velocity (or speed) in metres per second by multiplying it by itself. Eg. 1.4 x 1.4 = 1.96
@@ -72,9 +81,13 @@ try:
                 d = c*0.029
                 e = d*int(weight)
 
-                st.markdown(f"<h3 style='text-align: center; color: blue;'>You are wasting  {round(e, 2)} calories in per minute</h3>", unsafe_allow_html=True)
-            else:
-                st.markdown("<h3 style='text-align: center; color: blue;'>Please, stay at home!</h3>", unsafe_allow_html=True)
+                st.markdown(f"<h3 style='text-align: center; color: blue;'>You are walking and  wasting  {round(e, 2)} calories in per minute</h3>", unsafe_allow_html=True)
+            
+            elif pred==0:
+                met = 2.5
+                burned_per_minute = (met* weight*3.5)/ 200
+
+                st.markdown(f"<h3 style='text-align: center; color: blue;'>You are driving and  wasting  {round(burned_per_minute , 2)} calories in per minute</h3>", unsafe_allow_html=True)
 
 except ValueError:
     st.error('Please enter valid data type!')
@@ -82,5 +95,7 @@ except ValueError:
 about = st.sidebar.button("contributors")
 if about:
     st.title("Fayyozjon Usmonov")
+    st.title("Talvinder Singh")
+
     st.markdown("<h1 style='text-align: center; color: blue;'>Thanks for your attention!</h1>", unsafe_allow_html=True)
 
